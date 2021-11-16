89acf2061e3790c504729030bb748ea4b24adf81
Alissin82 <92631352+Alissin82@users.noreply.github.com>
2021-11-16 09:54:47 +0330

    rearrange

--- 2-6/php/footer.php
+++
@@ -1,3 +0,0 @@
-<footer class="text-black fixed-bottom" style="position:fixed;bottom:0;text-align:center;width:100%;padding:10px;box-sizing:border-box;z-index:10;">
-  تمام حقوق نرم افزار تحت مالکیت <strong>فراهنرجو</strong> است
-</footer>
